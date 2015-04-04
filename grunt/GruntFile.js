f95968ad80d05b1b1b56f980df59a182369d236f
Hauke Lampe <lampe@hauke-lampe.de>
2015-04-04 05:42:03 +0200

    preserve /*! comments in minified files, fwiw

--- grunt/GruntFile.js
+++ grunt/GruntFile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
 
 		uglify: {
 			options: {
-//				preserveComments: 'some',
+				preserveComments: 'some',
 //				banner: '/*! <%= gitinfo.local.branch.current.name %> / <%= gitinfo.local.branch.current.SHA %> */\n'
 			},
 
